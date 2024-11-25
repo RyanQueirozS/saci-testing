1321b3bd317466a4b8d934b55ca3b8d4f6b85e2b
RyanQueirozS <ryan@ryan>
2024-11-25 11:39:05 -0300

    doc: improved documentation

---
+++ README.md
@@ -0,0 +1,54 @@
+# Saci Testing
+
+Simply copy-paste/download the `saci-tessting.h` file to your project!
+
+# Example
+
+```c
+#define SACI_TEST_IMPLEMENTATION 
+
+#include "path-to/saci-testing.h" // define before including
+
+static void MyTestFunction() {
+    saci_Test_PrintPassed(true); // will print the passed tests. Default: false.
+    { // Test some functionality
+        saci_Test_ClockBegin(); // if you want to begin the clock!
+        
+        SACI_TEST_ASSERT(1 > 0, "One should be more than zero"); // SUCCESS
+        SACI_TEST_ASSERT(0 > 1, "One should be less than zero"); // FAIL
+    
+        saci_Test_ClockEnd();
+        SACI_TEST_ASSERT(saci_Test_GetElapsedTimeMS() < 5, "Should run in less then 5 ms");
+    }
+    {
+        for(int i = 0; i < 10; ++i) {
+            SACI_TEST_ASSERT(i < 5, "Number iteration should be less than 5"); 
+            // Should fail 5 times, but because the file, line number and 
+            // description is the same, it will only print once
+        }
+    }
+    saci_Test_End(); // Ends the test and prints the results
+}
+```
+
+You can change the macros:
+
+```c
+
+#define SACI_EPSILON 1e-8 
+// Default: 1e-6
+
+#define SACI_TEST_VEC3_IS_EQUAL(v1x,v1y,v1z,v2x,v2y,v2z)... 
+// Default is: SACI_TEST_VEC3_IS_EQUAL(v1,v2)
+
+// you can also change SACI_TEST_VEC2_IS_EQUAL, SACI_TEST_COLOR_IS_EQUAL,
+// SACI_RAND_RANGE_INT, SACI_RAND_RANGE_FLOAT
+
+// The SACI_TEST_ASSERT can also be changed
+#define SACI_TEST_ASSERT(condition)... 
+// Default is: SACI_TEST_ASSERT(condition, [description](description))
+
+#define SACI_TEST_MAXIMUM 10 // Will only have 10 tests. Will crash if there are more!
+// Default: 0x1000.
+
+```
--- saci-testing.h
+++ saci-testing.h
@@ -31,52 +31,141 @@
 #include <stdio.h>
 
 #ifndef SACI_EPSILON
+/**
+ * @def SACI_EPSILON
+ * @brief A small threshold value used for floating-point comparisons.
+ */
 #define SACI_EPSILON 1e-6
 #endif // SACI_EPSILON
 
 #ifndef SACI_TEST_VEC3_IS_EQUAL
+/**
+ * @def SACI_TEST_VEC3_IS_EQUAL
+ * @brief Macro to test if two 3D vectors are equal within a tolerance.
+ *
+ * @param v1 The first vector to compare, must have members `x`, `y`, and `z`.
+ * @param v2 The second vector to compare, must have members `x`, `y`, and `z`.
+ * @return A boolean value: true if the vectors are equal within the tolerance,
+ *         false otherwise.
+ */
 #define SACI_TEST_VEC3_IS_EQUAL(v1, v2)                                              \
     (fabs((v1.x) - (v2.x)) < SACI_EPSILON && fabs((v1.y) - (v2.y)) < SACI_EPSILON && \
      fabs((v1.z) - (v2.z)) < SACI_EPSILON)
 #endif // SACI_TEST_VEC3_IS_EQUAL
 
 #ifdef SACI_TEST_VEC2_IS_EQUAL
+/**
+ * @def SACI_TEST_VEC2_IS_EQUAL
+ * @brief Macro to test if two 2D vectors are equal within a tolerance.
+ *
+ * @param v1 The first vector to compare, must have members `x`, `y`.
+ * @param v2 The second vector to compare, must have members `x`, `y`.
+ * @return A boolean value: true if the vectors are equal within the tolerance,
+ *         false otherwise.
+ */
 #define SACI_TEST_VEC2_IS_EQUAL(v1, v2) \
     (fabs((v1.x) - (v2.x)) < SACI_EPSILON && fabs((v1.y) - (v2.y)) < SACI_EPSILON)
 #endif // SACI_TEST_VEC2_IS_EQUAL
 
 #ifndef SACI_TEST_COLOR_IS_EQUAL
+/**
+ * @def SACI_TEST_COLOR_IS_EQUAL
+ * @brief Macro to test if two 2D vectors are equal within a tolerance.
+ *
+ * @param c1 The first vector to compare, must have members `r`, `g`, 'b' and 'a'.
+ * @param c2 The second vector to compare, must have members `r`, `g`, 'b' and 'a'.
+ * @return A boolean value: true if the vectors are equal within the tolerance,
+ *         false otherwise.
+ */
 #define SACI_TEST_COLOR_IS_EQUAL(c1, c2)                                             \
     (fabs((c1.r) - (c2.r)) < SACI_EPSILON && fabs((c1.g) - (c2.g)) < SACI_EPSILON && \
      fabs((c1.b) - (c2.b)) < SACI_EPSILON && fabs((c1.a) - (c2.a)) < SACI_EPSILON)
 #endif // SACI_TEST_COLOR_IS_EQUAL
 
 #ifndef SACI_RAND_RANGE_INT
+/**
+ * @def SACI_RAND_RANGE_INT
+ * @brief Macro to generate a maximum integer value given a maximum range.
+ *
+ * @param max The maximum value the integer can be.
+ * @param goNegative A boolean value if the integer can go below 0.
+ * @return A integer value given the range and goNegative.
+ */
 #define SACI_RAND_RANGE_INT(max, goNegative) \
     ((goNegative) ? (rand() % ((max)*2 + 1) - (max)) : (rand() % ((max) + 1)))
 #endif // SACI_RAND_RANGE_INT
 
 #ifndef SACI_RAND_RANGE_FLOAT
+/**
+ * @def SACI_RAND_RANGE_FLOAT
+ * @brief Macro to generate a maximum float value given a maximum range.
+ *
+ * @param max The maximum value the float can be.
+ * @param goNegative A boolean value if the float can go below 0.
+ * @return A float value given the range and goNegative.
+ */
 #define SACI_RAND_RANGE_FLOAT(max, goNegative)                            \
     ((goNegative) ? (((float)rand() / (float)RAND_MAX) * (max)*2 - (max)) \
                   : (((float)rand() / (float)RAND_MAX) * (max)))
 #endif // SACI_RAND_RANGE_FLOAT
 
+/**
+ * @brief Ends the testing and prints the results.
+ */
 void saci_Test_End(void);
 
+/**
+ * @brief Enables or disables if should print passed values.
+ *
+ * @param enable A boolean to enable or disable printing.
+ */
 void saci_Test_PrintPassed(bool enable);
 
+/**
+ * @brief Creates and pushes a test to success. Should not be used by the end-user.
+ *
+ * @param func The function name, passed as __func__.
+ * @param file The file name, passed as __FILE__.
+ * @param line The line name, passed as __LINE__.
+ * @param description The test description.
+ */
 void saci_Test_Passed(char* func, char* file, int line, const char* description);
 
+/**
+ * @brief Creates and pushes a test to errors. Should not be used by the end-user.
+ *
+ * @param func The function name, passed as __func__.
+ * @param file The file name, passed as __FILE__.
+ * @param line The line name, passed as __LINE__.
+ * @param description The test description.
+ */
 void saci_Test_DidNotPass(char* func, char* file, int line, const char* description);
 
+/**
+ * @brief Begins the clock.
+ */
 void saci_Test_ClockBegin(void);
 
+/**
+ * @brief Ends the clock.
+ */
 void saci_Test_ClockEnd(void);
 
-double saci_GetElapsedTimeMS(void);
+/**
+ * @brief Returns the elapsed time from ClockBegin and ClockEnd.
+ *
+ * @return A double with the value of the time elapsed in ms.
+ */
+double saci_Test_GetElapsedTimeMS(void);
 
 #ifndef SACI_TEST_ASSERT
+/**
+ * @def SACI_TEST_ASSERT
+ * @brief checks if a condition is true.
+ *
+ * @param condition The condition to be evaluated.
+ * @param description The test description.
+ */
 #define SACI_TEST_ASSERT(condition, description)                                    \
     do {                                                                            \
         if (condition) {                                                            \
@@ -89,8 +178,12 @@ double saci_GetElapsedTimeMS(void);
 
 /* === Implementation === */
 
-#ifndef SACI_TEST_ERROR_MAXIMUM
-#define SACI_TEST_ERROR_MAXIMUM 0x1000
+#ifndef SACI_TEST_MAXIMUM
+/**
+ * @def SACI_TEST_MAXIMUM
+ * @brief The max value of successes and/or errors.
+ */
+#define SACI_TEST_MAXIMUM 0x1000
 #endif // SACI_TEST_ERROR_MAXIMUM
 
 static bool __printPassed = 0;
@@ -101,7 +194,7 @@ struct Success {
     char* file;
     int   line;
     char* description;
-} __successes[SACI_TEST_ERROR_MAXIMUM];
+} __successes[SACI_TEST_MAXIMUM];
 
 static size_t __errorIndex = 0;
 struct Error {
@@ -109,7 +202,7 @@ struct Error {
     char* file;
     int   line;
     char* description;
-} __errors[SACI_TEST_ERROR_MAXIMUM];
+} __errors[SACI_TEST_MAXIMUM];
 
 void saci_Test_End(void) {
     if (__printPassed) {
@@ -173,7 +266,7 @@ void saci_Test_ClockEnd(void) {
     __saci_testTimeEnd = clock();
 }
 
-double saci_GetElapsedTimeMS(void) {
+double saci_Test_GetElapsedTimeMS(void) {
     return (double)(1000.0f * (__saci_testTimeEnd - __saci_testTimeStart) / CLOCKS_PER_SEC);
 }
 
